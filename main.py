f7946891d7989e898527d994d89f68015377e47c
Federico Oliveri <federico.oliveri.22@um.edu.mt>
2024-05-19 10:46:28 +0200

    Added MonteCarlo + some updates

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
+import math
 
 CARD = {
-    "A": 1,
+    "A": 11,
     "2": 2,
     "3": 3,
     "4": 4,
@@ -50,7 +51,8 @@ class Deck:
 class User:
     def __init__(self):
         self.cards: list[Card] = []
-
+        self.usable_ace = False
+        
     def reset(self):
         self.cards = []
 
@@ -69,6 +71,11 @@ class User:
         while score > 21 and num_aces:
             score -= 10
             num_aces -= 1
+            
+        if num_aces >= 1:
+            self.usable_ace = True
+        else:
+            self.usable_ace = False
 
         return score
 
@@ -87,12 +94,13 @@ class Dealer(User):
 
 
 class PlayerRLAgent(Player):
-    def __init__(self, alpha=0.1, gamma=0.9, epsilon=0.1):
+    def __init__(self, alpha=0.1, gamma=0.9, epsilon=0.):
         super().__init__()
         self.alpha = alpha  # learning rate
         self.gamma = gamma  # discount factor
         self.epsilon = epsilon  # exploration rate
         self.q_table = {}  # Q(s, a) table
+        self.q_table_counts = {}  # Q(s, a) table for counts
 
     def choose_action(self, state):
         if random.random() < self.epsilon:
@@ -101,54 +109,89 @@ class PlayerRLAgent(Player):
             return self.get_best_action(state)  # exploit
 
     def get_best_action(self, state):
+        print("Choosing best action")
         if state not in self.q_table:
             print(f"State {state} not in Q-table")
             self.q_table[state] = {"h": 0.0, "s": 0.0}
         return max(self.q_table[state], key=self.q_table[state].get)
 
     def update_q_table(self, state, action, reward, next_state):
+        self.q_table_counts.setdefault(state, {}).setdefault(action, 0)
+        self.q_table_counts[state][action] += 1
+        
         if next_state is None:
             next_state_value = 0
         else:
             next_state_value = max(self.q_table.get(next_state, {"h": 0.0, "s": 0.0}).values())
+            print("\n*****Next state value = %5.2f*****\n" % (next_state_value))
+            
         current_value = self.q_table.get(state, {"h": 0.0, "s": 0.0}).get(action, 0.0)
         new_value = (1 - self.alpha) * current_value + self.alpha * (reward + self.gamma * next_state_value)
         self.q_table.setdefault(state, {"h": 0.0, "s": 0.0})[action] = new_value
+        
+    def print_q_table(self):
+        sorted_q_table = sorted(self.q_table.items(), key=lambda x: (x[0][0], x[0][1]))
+
+        for key, value in sorted_q_table:
+            print(key, value)
+        
+        print("__________________________________________")
+            
+        sorted_q_table_c = sorted(self.q_table_counts.items(), key=lambda x: (x[0][0], x[0][1]))
+
+        for key, value in sorted_q_table_c:
+            print(key, value)
 
 
 class BlackJackWithRL:
-    def __init__(self, deck: Deck, player, dealer):
-        self.deck: Deck = deck
-        self.player: Player = player
-        self.dealer: Dealer = dealer
+    def __init__(self, deck: Deck, player: Player, dealer: Dealer):
+        self.deck = deck
+        self.player = player
+        self.dealer = dealer
 
-    def play(self):
+    def play(self, choose_action_fn=None, episode_count=None):
         self.deck.shuffle()
         self.player.add_card(self.deck.draw_card())
         self.player.add_card(self.deck.draw_card())
         self.dealer.add_card(self.deck.draw_card())
-        state = (self.player.get_score(), self.dealer.cards[0].value)
 
         print("Player: ", self.player)
         print("Dealer: ", self.dealer)
+        
         # Player's turn
+        while self.player.get_score() < 12:
+            self.player.add_card(self.deck.draw_card())
+            print("Player [Automatic Draw]: ", self.player)
+
+        if self.player.get_score() == 21:
+            state = None
+
+        episode = []
+
         while self.player.get_score() < 21:
-            action = None
-            if isinstance(self.player, PlayerRLAgent):
-                state = (self.player.get_score(), self.dealer.cards[0].value)
+            state = (self.player.get_score(), self.dealer.cards[0].value, self.player.usable_ace)
+            if choose_action_fn:
+                action = choose_action_fn(state, episode_count, len(episode))
+            elif isinstance(self.player, PlayerRLAgent):
+                state = (self.player.get_score(), self.dealer.cards[0].value, self.player.usable_ace)
                 action = self.player.choose_action(state)
             else:
                 action = input("Do you want to hit or stand? (h/s): ")
+
+            episode.append((state, action))
 
             if action == "h":
                 self.player.add_card(self.deck.draw_card())
                 print("Player: ", self.player)
             elif action == "s":
+                print("Player has chosen to stand")
                 break
 
         if self.player.get_score() > 21:
-            print("Player busts! Dealer wins.")
-            if isinstance(self.player, PlayerRLAgent):
+            print("Player busts! Dealer wins.\n")
+            if choose_action_fn:
+                return episode, -1
+            elif isinstance(self.player, PlayerRLAgent):
                 self.player.update_q_table(state, action, -1, None)
             return
 
@@ -158,21 +201,38 @@ class BlackJackWithRL:
             print("Dealer: ", self.dealer)
 
         if self.dealer.get_score() > 21:
-            print("Dealer busts! Player wins.")
-            if isinstance(self.player, PlayerRLAgent):
+            print("Dealer busts! Player wins.\n")
+            if choose_action_fn:
+                return episode, 1
+            elif isinstance(self.player, PlayerRLAgent) and state is not None:
                 self.player.update_q_table(state, action, 1, None)
             return
 
         if self.player.get_score() > self.dealer.get_score():
-            print("Player wins!")
-            if isinstance(self.player, PlayerRLAgent):
+            print("Player wins!\n")
+            if choose_action_fn:
+                return episode, 1
+            elif isinstance(self.player, PlayerRLAgent) and state is not None:
                 self.player.update_q_table(state, action, 1, None)
         elif self.player.get_score() < self.dealer.get_score():
-            print("Dealer wins!")
-            if isinstance(self.player, PlayerRLAgent):
+            print("Dealer wins!\n")
+            if choose_action_fn:
+                return episode, -1
+            elif isinstance(self.player, PlayerRLAgent) and state is not None:
                 self.player.update_q_table(state, action, -1, None)
+            return
         else:
-            print("It's a tie!")
+            print("It's a tie!\n")
+            return episode, 0
+
+    def run_episode_mc(self, episode_count):
+        self.game_reset()
+        self.deck.shuffle()
+
+        choose_action_fn = lambda state, ep_count, ep_len: self.player.choose_action(state, ep_count, ep_len)
+
+        episode, reward = self.play(choose_action_fn, episode_count)
+        self.player.update_q_table_mc(episode, reward)
 
     def game_reset(self):
         self.player.reset()
@@ -180,14 +240,65 @@ class BlackJackWithRL:
         self.deck.reset()
 
     def start(self, num_episodes=1000):
-        for i in range(num_episodes):
-            print(f"Episode {i}")
-            self.game_reset()
-            self.play()
+        for episode_count in range(num_episodes):
+            print(f"Episode {episode_count}")
+            if isinstance(self.player, MonteCarloOnPolicyControl):
+                self.run_episode_mc(episode_count)
+            else:
+                self.game_reset()
+                self.play()
+
+                
+class MonteCarloOnPolicyControl(PlayerRLAgent):
+    def __init__(self, alpha=0.1, gamma=0.9, epsilon=0.1, exploring_starts=False, epsilon_config='1/k'):
+        super().__init__(alpha, gamma, epsilon)
+        self.exploring_starts = exploring_starts
+        self.epsilon_config = epsilon_config
+        self.returns_sum = {}
+        self.returns_count = {}
+
+    def choose_action(self, state, episode_count, episode):
+        if self.exploring_starts and state[0] in range(12, 21) and len(episode) == 1:
+            print("Explore!")
+            return random.choice(["h", "s"])
+        else:
+            print("Exploit!!")
+            epsilon = self.calculate_epsilon(episode_count)
+            if random.random() < epsilon:
+                print("Epsilon!!")
+                return random.choice(["h", "s"])
+            else:
+                return self.get_best_action(state)
+
+    def calculate_epsilon(self, episode_count):
+        if self.epsilon_config == '1/k':
+            return 1 / (episode_count + 1)
+        elif self.epsilon_config == 'e^(-k/1000)':
+            return math.exp(-episode_count / 1000)
+        elif self.epsilon_config == 'e^(-k/10000)':
+            return math.exp(-episode_count / 10000)
+        else:
+            raise ValueError("Invalid epsilon configuration")
+
+    def update_q_table_mc(self, episode, reward):
+        visited_states_actions = set()
+        for state, action in episode:
+            self.q_table_counts.setdefault(state, {}).setdefault(action, 0)
+            self.q_table_counts[state][action] += 1
+        
+            if (state, action) not in visited_states_actions:
+                visited_states_actions.add((state, action))
+                if (state, action) not in self.returns_sum:
+                    self.returns_sum[(state, action)] = 0.0
+                    self.returns_count[(state, action)] = 0
+                self.returns_sum[(state, action)] += reward
+                self.returns_count[(state, action)] += 1
+                self.q_table.setdefault(state, {"h": 0.0, "s": 0.0})
+                self.q_table[state][action] = self.returns_sum[(state, action)] / self.returns_count[(state, action)]
 
 
-# Create RL agents for player and  create the dealer
-player = PlayerRLAgent()
+# Create RL agents for player and create the dealer
+player = MonteCarloOnPolicyControl()
 dealer = Dealer()
 
 deck = Deck()
@@ -195,4 +306,6 @@ print(f"Deck of {len(deck.cards)} cards created")
 
 # Create Blackjack game with RL agents
 game_with_rl = BlackJackWithRL(deck, player, dealer)
-game_with_rl.start(100)
+game_with_rl.start(1000)
+
+player.print_q_table()
